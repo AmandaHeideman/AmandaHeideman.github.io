56895549c4aa0515cd21864cc936a9d97c0b5058
Amanda Heideman <heideman.amanda@gmail.com>
2020-11-15 23:16:56 +0100

    wrote rapport.txt, added text and img to projects in portfolio

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function (e){
                         "Det här projektet fokuserade främst på javascript, där även den mesta html:en genererades"+
                         " utifrån hur många spelare som valts.";
 
+                        //Lägger till bild
                         let fig = document.createElement("figure");
                         let img = document.createElement("img");
                         img.setAttribute("src", "/images/yatzy_game.PNG");
@@ -96,8 +97,22 @@ document.addEventListener("DOMContentLoaded", function (e){
                         //Lägger till info
                         let text = document.createElement("p");
                         text.innerHTML = "Quizet var ett individuellt projekt där vi skulle med"+
-                        " hjälp av ett API hämta frågor till själva quizet. "
+                        " hjälp av ett API (quizapi.io) hämta frågor till själva quizet. Spelaren"+
+                        " skulle sen få välja mellan 5-10 frågor, som kunde ha ett eller flera rätta"+
+                        " svar. När spelet var slut skulle spelarens poäng skrivas ut, och spelaren"+
+                        " skulle få valet att spela igen. <br><br>"+
+                        "Det här projektet var mest en övning i att använda och hatera klasser i javascript."+
+                        " Vi fick också lära oss att använda fetch eftersom frågorna hämtades från ett API."
     
+                        //Lägger till bild
+                        let fig = document.createElement("figure");
+                        let img = document.createElement("img");
+                        img.setAttribute("src", "/images/quiz_color.png");
+                        img.setAttribute("alt", "Skärmklipp av pågående spel");
+                        let caption = document.createElement("figcaption");
+                        caption.innerHTML = "Skärmklipp av pågående spel";
+                        fig.appendChild(img).after(caption);
+                        
                         //Länkar
                         let github = document.createElement("a");
                         github.setAttribute("href", "https://github.com/amandaheideman/quiz");
@@ -111,7 +126,7 @@ document.addEventListener("DOMContentLoaded", function (e){
                         back.setAttribute("href", "portfolio.html");
                         back.innerHTML = "&gt; Tillbaka";
                         
-                        element.appendChild(text);
+                        element.appendChild(fig).after(text);
                         element.appendChild(github).after(game);
                         element.appendChild(back);
                     }
@@ -128,7 +143,30 @@ document.addEventListener("DOMContentLoaded", function (e){
                     else{
                         element.classList.add("show");
                         element.classList.remove("ACME");
+                        //Lägger till info
+                        let text = document.createElement("p");
+                        text.innerHTML = "ACME Events var ett grupparbete i kursen Frontend 2 -"+
+                        " HTML och CSS. Projektet gick ut på att göra en hemsida som listar events."+
+                        " Det skulle finnas en förstasida med ett par utvalda evenemang, en sida där"+
+                        " evenemangen listas, en detaljsida för enskilda evenemang, samt en om oss-sida,"+
+                        " där vi skulle beskriva den agila arbetsmetoden Scrum, som vi lärde oss att"+
+                        " använda i det här projektet. Det skulle också finnas ett administrations-"+
+                        " gränssnitt, som tillät en admin att CRUD:a (Create, Read, Update, Delete)"+
+                        " evenemang. Man skulle också som användare kunna sortera evenemang efter exempelvis"+
+                        " datum eller kategori. <br><br>"+
+                        "I det här grupparbetet lärde vi oss att arbeta efter en grafisk profil, samt"+
+                        " arbetsmetoden Scrum. Vi lärde oss också bla att använda local storage."
     
+                        //Lägger till bild
+                        let fig = document.createElement("figure");
+                        let img = document.createElement("img");
+                        img.setAttribute("src", "/images/ACME_site.png");
+                        img.setAttribute("alt", "Skärmklipp av hemsidans evenemangslista");
+                        img.setAttribute("class", "acmeImg")
+                        let caption = document.createElement("figcaption");
+                        caption.innerHTML = "Skärmklipp av hemsidans evenemangslista, där admin-verktygen syns";
+                        fig.appendChild(img).after(caption);
+                        
                         //Länkar
                         let github = document.createElement("a");
                         github.setAttribute("href", "https://github.com/Hano2001/ACME-Grupp-4");
@@ -143,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function (e){
                         back.setAttribute("href", "portfolio.html");
                         back.innerHTML = "&gt; Tillbaka";
                         
-                        // element.appendChild(text);
+                        element.appendChild(text).after(fig);
                         element.appendChild(github).after(game);
                         element.appendChild(back);
                     }
@@ -160,6 +198,24 @@ document.addEventListener("DOMContentLoaded", function (e){
                     else{
                         element.classList.add("show");
                         element.classList.remove("memory");
+
+                        //Lägger till info
+                        let text = document.createElement("p");
+                        text.innerHTML = "Det här projektet var inte ett skolprojekt, utan ett litet "+
+                        "mini-projekt jag gjorde lite snabbt på fritiden. Jag följde en Youtube-tutorial, "+
+                        "Learn JavaScript by Building 7 Games av freeCodeCamp.org (länk längre ner), satte "+
+                        "egna bilder som jag letade upp på nätet och skalade om, och la till extra som jag "+
+                        "ville ha med, som en highscore, en slutbild, och en del CSS."
+                        
+                        //lägger till bild
+                        let fig = document.createElement("figure");
+                        let img = document.createElement("img");
+                        img.setAttribute("src", "/images/memory_game.png");
+                        img.setAttribute("alt", "Skärmklipp av pågående spel");
+                        img.classList.add("memoryImg");
+                        let caption = document.createElement("figcaption");
+                        caption.innerHTML = "Skärmklipp av pågående spel";
+                        fig.appendChild(img).after(caption);
     
                         //Länkar
                         let github = document.createElement("a");
@@ -169,14 +225,18 @@ document.addEventListener("DOMContentLoaded", function (e){
                         let game = document.createElement("a");
                         game.setAttribute("href", "https://amandaheideman.github.io/Memory");
                         game.innerHTML = "&gt; Länk till spelet";
+
+                        let youtube = document.createElement("a");
+                        youtube.setAttribute("href", "https://www.youtube.com/watch?v=lhNdUVh3qCc");
+                        youtube.innerHTML = "&gt; Länk till Youtube-tutorial";
     
                         let back = document.createElement("a");
                         back.setAttribute("href", "portfolio.html");
                         back.innerHTML = "&gt; Tillbaka";
 
-                        //element.appendChild(text);
+                        element.appendChild(fig).after(text);
                         element.appendChild(github).after(game);
-                        element.appendChild(back);
+                        element.appendChild(youtube).after(back);
                     }
             
                 })
